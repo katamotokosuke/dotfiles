620739b6cfdce473280ff333934a1ab261ecc68b
katamoto_kosuke <katamoto_kosuke@uniquevision.co.jp>
2017-10-16 14:26:26 +0900

    add emacs config file

---
+++ emacs.d/init.el
@@ -0,0 +1,10 @@
+(require 'linum)
+
+;;行番号表示
+(global-linum-mode)
+;;バックアップファイル作成しない
+(setq make-backup-file nil)
+;;start up 画面を出さない
+(setq inhibit-startup-screen t)
+;;メニューバーを消す
+(tool-bar-mode 0)
