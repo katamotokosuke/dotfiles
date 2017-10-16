76883d222dccbfeb326396df4e19ad0045899f6e
katamoto_kosuke <katamoto_kosuke@uniquevision.co.jp>
2017-10-16 20:30:51 +0900

    use emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,9 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (require 'linum)
 
 ;;行番号表示
@@ -8,3 +14,6 @@
 (setq inhibit-startup-screen t)
 ;;メニューバーを消す
 (tool-bar-mode 0)
+
+;;補完
+;;(global-company-mode)
